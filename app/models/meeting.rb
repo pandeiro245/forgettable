83019d2c80fc8ee30233139cb7cbe29f92b22d51
西小倉宏信 <nishiko@mindia.jp>
2018-08-20 14:34:53 +0000

    divide methods of Meeting

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -4,34 +4,55 @@ Zoomus.configure do |c|
 end
 
 class Meeting
-  attr_accessor :user
-
-  def initialize
+  def initialize(user_name=nil, target_day = nil)
     @zoom = Zoomus.new
-    user_list = @zoom.user_list
-    @host = user_list['users'].first
+    @host_id = ENV['ZOOM_HOST']
+    @user_name = user_name || ENV['ZOOM_USERNAME']
+    @target_day = target_day || Date.today.prev_month
   end
 
-  def report(user_id, target_day=nil, page_number=1)
-    target_day ||= Date.today.prev_month
-    @zoom.report_getuserreport(
-      user_id: user_id,
-      from: target_day.beginning_of_month,
-      to: target_day.end_of_month,
+  def monthly_amount
+    monthly_hour.to_i * 1000
+  end
+
+  def monthly_hour
+    target_minutes.reduce(:+)/3600
+  end
+
+  # private
+  def target_minutes
+    target_meetings.map do |meeting|
+      user = meeting['participants'].select{|user| user['name'] == @user_name}.first
+      user['leave_time'].to_datetime.to_f - user['join_time'].to_datetime.to_f
+    end
+  end
+
+  def target_meetings
+    meetings.select do |meeting|
+      meeting['participants'].select do |user|
+        user['name'] == @user_name
+      end.present?
+    end
+  end
+
+  def meetings
+    user_report['meetings']
+  end
+
+  def user_report(page_number=1)
+    res = @zoom.report_getuserreport(
+      user_id: @host_id,
+      from: @target_day.beginning_of_month,
+      to: @target_day.end_of_month,
       page_size: 300,
       page_number: page_number # TODO: need to count up
     )
-  end
-
-
-  def data
-    zoom_data['meetings']
-  end
-
-  private
-
-  def zoom_data
-    user_id = @host['id']
-    zoomus_client.meeting_list(:host_id => user_id)
+    if res['error']
+      puts res['error']['message']
+      sleep 10
+      user_report(page_number)
+    else
+      res
+    end
   end
 end
