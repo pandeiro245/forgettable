b05a134f936a0954bc548c3d9c7692e2deb37fc8
zu-min <okka.0928@gmail.com>
2018-10-12 18:13:45 +0900

    add team_reports of timecrowd api

--- lib/timecrowd_api.rb
+++ lib/timecrowd_api.rb
@@ -5,6 +5,15 @@ class TimecrowdApi
       call_api(:get, url, token)
     end
 
+    def team_reports(team_id: nil, month: nil, token: nil)
+      month ||= Date.today
+      from = month.beginning_of_month.strftime('%Y-%m-%d')
+      to   = month.end_of_month.strftime('%Y-%m-%d')
+
+      url = "/api/v1/teams/#{team_id}/report/team?user_id=null&from=#{from}&to=#{to}"
+      call_api(:get, url, token)
+    end
+
     def report_periods(token)
       url = '/api/v1/reports/periods'
       call_api(:get, url, token)
